e52257b15e57aeb587474e2a9315a2d65c0f09c8
Misha Milovidov <mmilovidov@doterra.com>
2016-10-24 11:42:00 -0600

    Finished Rental View

--- Controllers/RentalsController.cs
+++ Controllers/RentalsController.cs
@@ -17,14 +17,14 @@ namespace BlowOut.Controllers
         	_context = context;
     	}
 
-    	// GET: /<controller>/
-    	public IActionResult Index()
-    	{
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
             IEnumerable<RentalInfo> instruments = _context.RentalInfo.FromSql(
-            	"SELECT Instruments.InstrumentId, Instruments.InstrumentName, Instruments.InstrumentPrice, Rental_Types.RentalTypeID, Rental_Types.RentalType, Instrument_Pictures.InstrumentPictureID, Instrument_Pictures.InstrumentPicture FROM Instruments INNER JOIN Rental_Types ON Rental_Types.RentalTypeID = Instruments.RentalTypeID INNER JOIN Instrument_Pictures ON Instrument_Pictures.InstrumentPictureID = Instruments.InstrumentPictureID"
-            	);
+                "SELECT Instruments.InstrumentId, Instruments.InstrumentName, Instruments.InstrumentPrice, Rental_Types.RentalTypeID, Rental_Types.RentalType, Instrument_Pictures.InstrumentPictureID, Instrument_Pictures.InstrumentPicture FROM Instruments INNER JOIN Rental_Types ON Rental_Types.RentalTypeID = Instruments.RentalTypeID INNER JOIN Instrument_Pictures ON Instrument_Pictures.InstrumentPictureID = Instruments.InstrumentPictureID"
+                );
 
-        	return View(instruments);
-    	}
+            return View(instruments);
+        }
     }
 }
